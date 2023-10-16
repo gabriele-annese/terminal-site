6fd452f50177dddddd0d0e20812b93762bbf4807
Gabriele Annese <gannese06@gmail.com>
2023-10-16 23:43:15 +0200

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './components/styles/App.css';
+
+function App() {
+  // js logic
+  var Title = "Hello World!!"
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{Title}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
